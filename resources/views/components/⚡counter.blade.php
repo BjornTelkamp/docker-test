9a3afcc499c85a30cdc056e176176b9a8f3a41dd
Bjorn Telkamp <bjorntelkamp2000@gmail.com>
2026-01-30 20:41:18 +0100

    counter styling update

--- resources/views/components/⚡counter.blade.php
+++ resources/views/components/⚡counter.blade.php
@@ -6,7 +6,7 @@ use Livewire\Component;
 new class extends Component {
     public int $counter;
 
-    public function mount()
+    public function mount(): void
     {
         $this->counter = Cache::get('counter', 0);
     }
@@ -28,31 +28,37 @@ new class extends Component {
         }
     }
 
-    private function updateCache(string $variation): void
+    public function resetCounter()
     {
-        if ($this->counter === 0) {
-            Cache::add('counter', 1);
-        }
-        Cache::$variation('counter', 1);
+        Cache::set('counter', 0);
+        $this->counter = 0;
     }
 };
 ?>
 
-<div class="h-screen bg-[#F9E4BC] mx-auto">
+<div class="h-screen bg-[#F9E4BC] mx-auto select-none">
     <div class="flex justify-center flex-col h-[75vh]">
         <div class="flex flex-col gap-20">
             <div class="text-9xl font-bold text-center text-green-950">
                 {{ $this->counter }}
             </div>
-            <div class="flex justify-center gap-2">
-                <button wire:click="increaseCount"
-                        class="text-light rounded p-2 bg-lime-800 hover:bg-lime-900 active:ring-2 active:ring-offset-2 active:ring-lime-900 text-zinc-100 active:ring-offset-[#F9E4BC]">
-                    Increase count
-                </button>
-                <button wire:click="decreaseCount"
-                        class="text-light rounded p-2 bg-amber-400 hover:bg-amber-500 active:ring-2 active:ring-offset-2 active:ring-amber-500 text-zinc-800 active:ring-offset-[#F9E4BC]">
-                    Decrease count
-                </button>
+            <div class="flex flex-col justify-center gap-4">
+                <div class="flex justify-center gap-2">
+                    <button wire:click="increaseCount"
+                            class="text-light rounded p-2 bg-lime-800 hover:bg-lime-900 hover:cursor-pointer active:ring-2 active:ring-offset-2 active:ring-lime-900 text-zinc-100 active:ring-offset-[#F9E4BC]">
+                        Increase count
+                    </button>
+                    <button wire:click="decreaseCount"
+                            class="text-light rounded p-2 bg-amber-400 hover:bg-amber-500 hover:cursor-pointer active:ring-2 active:ring-offset-2 active:ring-amber-500 text-zinc-800 active:ring-offset-[#F9E4BC]">
+                        Decrease count
+                    </button>
+                </div>
+                <div class="flex justify-center">
+                    <button wire:click="resetCounter"
+                            class="text-light rounded p-2 bg-red-600 hover:bg-red-700 hover:cursor-pointer active:ring-2 active:ring-offset-2 active:ring-red-700 text-zinc-100 active:ring-offset-[#F9E4BC]">
+                        Reset counter
+                    </button>
+                </div>
             </div>
         </div>
     </div>
